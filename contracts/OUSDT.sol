0cde44fff49b3789cf0bac525c2e8277957babb3
DionysusBenstein <dionysusbenstein@gmail.com>
2023-05-04 21:54:27 +0300

    first commit

---
+++ contracts/OUSDT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OUSDT is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000 * (10 ** 18));
+    }
+
+    mapping(address => uint) public lockTime;
+
+    function requestTokens(address requestor , uint amount) external {
+        // require(block.timestamp > lockTime[msg.sender], "lock time has not expired. Please try again later");
+        _mint(requestor, amount * (10 ** 18));
+        // lockTime[msg.sender] = block.timestamp + 1 days;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  const OUSDT = await hre.ethers.getContractFactory("OUSDT");
+  const token = await OUSDT.deploy("OUSDT", "OUSDT");
+
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
